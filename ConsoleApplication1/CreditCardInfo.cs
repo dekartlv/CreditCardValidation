6a8bd802126f5f1f668b882faaf7d70fab95c7c8
dekartlv <dekartlv@gmail.com>
2016-05-05 20:09:24 +0300

    Working console application.

---
+++ ConsoleApplication1/CreditCardInfo.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+
+namespace CreditCardValidationApp
+{
+    public static class CreditCardInfo
+    {
+        public enum CardType {Visa,MasterCard,Maestro,Amex,JCB,Unknown};
+
+        private static string FormatCardNumber(string cardNumberInput)
+        {
+            if (cardNumberInput == null)
+                throw new ArgumentNullException(cardNumberInput, "Card number has no reference to an actual object");
+
+            string cardNumberClean = cardNumberInput;
+            if (cardNumberInput.Contains("-")) cardNumberClean = cardNumberInput.Replace("-", "");
+            if (cardNumberInput.Contains(" ")) cardNumberClean = cardNumberInput.Replace(" ", "");
+
+            return cardNumberClean;
+        }
+        
+        public static string GetCreditCardVendor(string cardNumberInput)
+        {
+            string cardNumberClean = FormatCardNumber(cardNumberInput);
+            switch (cardNumberClean[0]-'0')
+            {
+                case 3:
+                    if (int.Parse(cardNumberClean.Substring(0, 4)) >= 3528 && int.Parse(cardNumberClean.Substring(0, 4)) <= 3589)
+                        return CardType.JCB.ToString();
+                    if (int.Parse(cardNumberClean.Substring(0, 2)) == 34 || int.Parse(cardNumberClean.Substring(0, 2)) == 37)
+                        return CardType.Amex.ToString();
+                    else return CardType.Unknown.ToString();
+                case 4: return CardType.Visa.ToString();
+                case 5:
+                    if (int.Parse(cardNumberClean.Substring(0, 2)) >= 51 && int.Parse(cardNumberClean.Substring(0, 2)) <= 55)
+                        return CardType.MasterCard.ToString();
+                    else if (int.Parse(cardNumberClean.Substring(0, 2)) == 50 ||
+                        (int.Parse(cardNumberClean.Substring(0, 2)) >= 56 && int.Parse(cardNumberClean.Substring(0, 2)) <= 69))
+                        return CardType.Maestro.ToString();
+                    else return CardType.Unknown.ToString();
+                default: return CardType.Unknown.ToString();
+            }
+        }
+
+        public static bool IsCreditCardNumberValid(string cardNumberInput)
+        {
+            string cardNumberClean = FormatCardNumber(cardNumberInput);
+            cardNumberClean = new string(cardNumberClean.ToCharArray().Reverse().ToArray());
+
+            int digit = 0, sum = 0, total = 0;
+            for (int i = 0; i < cardNumberClean.Length; i++)
+            {
+                digit = cardNumberClean[i] - '0';
+                if (i % 2 == 1)
+                    sum = ((digit * 2) > 9) ? (digit * 2 - 9) : digit * 2;
+                else sum = digit;
+                total += sum;
+            }
+           
+            return total % 10 == 0;
+        }
+
+        public static string GenerateNextCreditCardNumber(string cardNumberInput)
+        {
+            string cardNumberClean = FormatCardNumber(cardNumberInput);
+            long nextCreditCardNumber = long.Parse(cardNumberClean);
+            nextCreditCardNumber++;
+
+            while (IsCreditCardNumberValid(nextCreditCardNumber.ToString()) == false)
+            {
+                nextCreditCardNumber++;
+            }
+
+            return nextCreditCardNumber.ToString();
+        }
+
+        public static string GenerateRandomCreditCardNumber(CardType cardType, int numberLength = 16)
+        {
+            string cardNumber = "";
+            decimal temp = 0;
+            switch (cardType)
+            {
+                case CardType.Visa:
+                    //numberLength for Visa credit cards could be 13 or 16 digits.
+                    cardNumber += 4;
+                    break;
+                case CardType.MasterCard:
+                    cardNumber += Math.Floor((decimal)new Random().Next(51, 56));
+                    break;
+                case CardType.Maestro:
+                    //numberLength for Maestro credit cards could vary from 14 till 19 digits.
+                    temp = Math.Floor((decimal)new Random().Next(55, 70));
+                    if (temp == 55)
+                        cardNumber += 50;
+                    else cardNumber += temp;
+                    break;
+                case CardType.Amex:
+                    temp = Math.Floor((decimal)new Random().Next(1));
+                    if (temp == 0)
+                        cardNumber += 34;
+                    else cardNumber += 37;
+                    numberLength = 15;
+                    break;
+                case CardType.JCB:
+                    cardNumber += Math.Floor((decimal)new Random().Next(3528, 3589));
+                    break;
+                    //default: return "";
+            }
+
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            var iterations = numberLength - cardNumber.Length - 1;
+            for (int i = 0; i < iterations; i++)
+            {
+                cardNumber += rnd.Next(10);
+            }
+            var checkDigit = cardNumber.Select((d, i) => i % 2 == cardNumber.Length % 2 ? ((2 * d) % 10) + d / 5 : d).Sum() % 10;
+
+            cardNumber = GenerateNextCreditCardNumber(cardNumber);
+            return cardNumber.ToString();
+        }
+     
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace CreditCardValidationApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            string[] cards = new string[] {
+                //http://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
+                "6706 0973 9510 7220",
+                "5186 0097-9483 6847",
+                "49-927-398-716",
+                "3782-8224 6310-005",  // American Express
+                "4012888888881881", // Visa
+                "4222222222222", // Visa
+                "30569309025904", // Diners Club 
+                "5147004213414803", // Mastercard
+                "379616680189541", // American Express
+                "4916111026621797", // Visa
+            };
+
+            foreach (string card in cards)
+            {
+                Console.WriteLine("================================");
+                Console.WriteLine("Card number: " + card);
+                Console.WriteLine("Card vendor: " + CreditCardInfo.GetCreditCardVendor(card));
+                Console.WriteLine("Is valid: " + CreditCardInfo.IsCreditCardNumberValid(card));
+                Console.WriteLine();
+            }
+
+            var newCard = CreditCardInfo.GenerateNextCreditCardNumber("379616680189541");
+            Console.WriteLine("===================================");
+            Console.WriteLine("New card number: " + newCard);
+            Console.WriteLine("New card's vendor: " + CreditCardInfo.GetCreditCardVendor(newCard));
+            Console.WriteLine("New card is valid: " + CreditCardInfo.IsCreditCardNumberValid(newCard));
+            Console.WriteLine();
+
         }
     }
 }
